a3bcbedff689f0b8794f026ca573d9c6022d1f67
reham elnaggar <rehamelsayedelnaggar@gmail.com>
2024-07-02 16:56:57 +0300

    fisrt to homePage cafe

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\HomeCafeController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homePage');
 });
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('/homePage', [HomeCafeController::class, 'homePage'])->name('homePage');
