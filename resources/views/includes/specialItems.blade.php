c05e4f2bb33f6d898fc0fcd9db4730552955c549
reham elnaggar <rehamelsayedelnaggar@gmail.com>
2024-07-24 01:22:28 +0300

    update

--- resources/views/includes/specialItems.blade.php
+++ resources/views/includes/specialItems.blade.php
@@ -2,7 +2,6 @@
     @foreach ($specialItems as $item)
         <div class="tm-black-bg tm-special-item">
             <img src="{{ asset('assets/img/' . $item->image) }}" alt="{{ $item->name }}">
-            <p>{{ asset('assets/img/' . $item->image) }}</p> 
             <div class="tm-special-item-description">
                 <h2 class="tm-text-primary tm-special-item-title">{{ $item->name }}</h2>
                 <p class="tm-special-item-text">{{ $item->description }}</p>  
